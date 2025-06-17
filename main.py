048c51196ec0579891968e6b015785c8aba7d137
AI Jeshua <josh@honest.house>
2025-06-17 14:27:54 -0700

    Ignore books (data) and write function: get and print the book text

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+
+main()
